bb026dcf7514d8bd1c7f5a98195ad5f713990ddb
timeyyy <timeyyy_da_man@hotmail.com>
2016-08-18 22:27:51 +0200

    tests using a run method to access api

--- tests/unit/test_core_requests.py
+++ tests/unit/test_core_requests.py
@@ -42,41 +42,48 @@ class MixIn():
             pass
         dbapi.new_db(self.core.db)
 
+    def run(self, func, *args, **kwargs):
+        '''
+        some isolation if we change from generaotrs
+        '''
+        res = list(func(*args, **kwargs))
+        if len(res) == 1:
+            return res[0]
+        return res
 
-@pytest.mark.k
+
 class TestProfile(MixIn):
     def test_profile_create(self):
-        import pdb;pdb.set_trace()
-        response = req.profile_create(self.core, 'new')
+        response = self.run(req.profile_create, self.core, 'new')
         assert 'profile created' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] == 1
 
-        import pdb;pdb.set_trace()
-        response = req.profile_create(self.core, 'new')
+        response = self.run(req.profile_create, self.core, 'new')
         assert 'profile already' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] != 1
 
 
     def test_profile_delete(self):
-        response = req.profile_delete(self.core, 'bad_profile')
+        response = self.run(req.profile_delete, self.core, 'bad_profile')
         assert 'does not exist' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] != 1
 
-        response = req.profile_create(self.core, 'new')
+        response = self.run(req.profile_create, self.core, 'new')
 
-        response = req.profile_delete(self.core, 'new')
+        response = self.run(req.profile_delete, self.core, 'new')
         assert 'profile deleted' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] == 1
 
 
     def test_profile_set_and_get_active(self):
         profile = 'test'
-        req.profile_create(self.core, profile)
-        response = req.profile_set_active(self.core, profile)
+        self.run(req.profile_create, self.core, profile)
+        response = self.run(req.profile_set_active, self.core, profile)
         assert 'profile changed' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] == 1
+        data = get_all_data(self.core.db, 'Profiles')
 
-        response = req.profile_get_active(self.core)
+        response = self.run(req.profile_get_active, self.core)
         abc = response
         response['output_method'] == 'noop'
         cond = "where Name == '{}'".format('default')
@@ -95,7 +102,7 @@ class TestBook(MixIn):
         req.profile_create(self.core, self.profile)
 
     def test_book_create(self):
-        response = req.book_create(self.core, self.profile, self.book,
+        response = self.run(req.book_create, self.core, self.profile, self.book,
                                    self.hotkey, self.active_profile)
         assert 'book created' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] == 1
@@ -103,38 +110,38 @@ class TestBook(MixIn):
         assert data[0] == self.book
         assert data[1] == self.profile
 
-        response = req.book_create(self.core, self.profile, self.book,
+        response = self.run(req.book_create, self.core, self.profile, self.book,
                                    self.hotkey, self.active_profile)
         assert 'book already' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] != 1
 
         book2 = 'asdf'
-        response = req.book_create(self.core, self.profile, book2,
+        response = self.run(req.book_create, self.core, self.profile, book2,
                                    self.hotkey, self.active_profile)
         assert 'hotkey already' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] != 1
 
-        response = req.book_create(self.core, self.profile, book2,
+        response = self.run(req.book_create, self.core, self.profile, book2,
                                    '', self.active_profile)
         assert 'hotkey is required' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] != 1
 
 
     def test_book_delete(self):
-        response = req.book_delete(self.core, 'bad_book', self.profile)
+        response = self.run(req.book_delete, self.core, 'bad_book', self.profile)
         assert 'does not exist' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] != 1
 
-        response = req.book_create(self.core, self.profile, self.book,
+        response = self.run(req.book_create, self.core, self.profile, self.book,
                                    self.hotkey, self.active_profile)
 
-        response = req.book_delete(self.core, self.book, self.profile)
+        response = self.run(req.book_delete, self.core, self.book, self.profile)
         assert 'book deleted' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] == 1
         data = get_all_data(self.core.db, 'Books')
         assert not data
 
-        response = req.book_delete(self.core, self.book, self.profile)
+        response = self.run(req.book_delete, self.core, self.book, self.profile)
         assert 'does not exist' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] != 1
 
@@ -145,7 +152,7 @@ class TestPage(MixIn):
 
     def test_page_create(self):
         specific = None;
-        response = req.page_create(self.core, self.profile, self.book,
+        response = self.run(req.page_create, self.core, self.profile, self.book,
                                    self.program, specific)
         assert 'page created' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] == 1
@@ -154,45 +161,45 @@ class TestPage(MixIn):
         assert data[2] == self.book
         assert data[3] == self.program
 
-        response = req.page_create(self.core, self.profile, self.book,
+        response = self.run(req.page_create, self.core, self.profile, self.book,
                                    self.program, specific)
         assert 'page already' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] != 1
 
     def test_page_delete(self):
         specific = None; loose = None
-        response = req.page_delete(self.core, self.profile, self.book,
+        response = self.run(req.page_delete, self.core, self.profile, self.book,
                                     'bad_page', specific, loose)
         assert 'does not exist' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] != 1
 
-        response = req.page_create(self.core, self.profile, self.book,
+        response = self.run(req.page_create, self.core, self.profile, self.book,
                                    self.program, specific)
 
 
-        response = req.page_delete(self.core, self.profile, self.book,
+        response = self.run(req.page_delete, self.core, self.profile, self.book,
                                     self.program, specific, loose)
         assert 'page deleted' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] == 1
         data = get_all_data(self.core.db, 'Pages')
         assert not data
 
-        response = req.page_delete(self.core, self.profile, self.book,
+        response = self.run(req.page_delete, self.core, self.profile, self.book,
                                     self.program, specific, loose)
         assert 'does not exist' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] != 1
 
     def test_page_update(self):
         specific = None; loose = None
-        response = req.page_update(self.core, self.profile, self.book,
+        response = self.run(req.page_update, self.core, self.profile, self.book,
                                     'bad_page', specific, loose)
         assert 'does not exist' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] != 1
 
-        response = req.page_create(self.core, self.profile, self.book,
+        response = self.run(req.page_create, self.core, self.profile, self.book,
                                    self.program, specific)
 
-        response = req.page_update(self.core, self.profile, self.book,
+        response = self.run(req.page_update, self.core, self.profile, self.book,
                                    self.program, specific)
         assert 'page saved' in response['output_kwargs']['msg'].lower()
         assert response['output_kwargs']['code'] == 1
@@ -205,56 +212,56 @@ class TestPage(MixIn):
 class TestHotkeys(MixIn):
 
     def test_hotkey_create(self):
-        response = req.hotkey_create(self.core, self.profile, self.book,
+        response = self.run(req.hotkey_create, self.core, self.profile, self.book,
                                      self.hotkey)
         assert 'created' in response['key']
 
-        response = req.hotkey_create(self.core, self.profile, self.book,
+        response = self.run(req.hotkey_create, self.core, self.profile, self.book,
                                      self.hotkey)
         assert 'taken' in response['key']
 
 
     def test_hotkey_delete(self):
-        response = req.hotkey_delete(self.core, self.profile, self.book,
+        response = self.run(req.hotkey_delete, self.core, self.profile, self.book,
                                      self.hotkey)
         assert 'not_found' in response['key']
 
-        response = req.hotkey_create(self.core, self.profile, self.book,
+        response = self.run(req.hotkey_create, self.core, self.profile, self.book,
                                      self.hotkey)
 
-        response = req.hotkey_delete(self.core, self.profile, self.book,
+        response = self.run(req.hotkey_delete, self.core, self.profile, self.book,
                                      self.hotkey)
         assert 'deleted' in response['key']
 
 
     def test_hotkey_updated(self):
-        response = req.hotkey_update(self.core, self.profile, self.book,
+        response = self.run(req.hotkey_update, self.core, self.profile, self.book,
                                      self.hotkey)
         assert 'not_found' in response['key']
 
-        response = req.hotkey_create(self.core, self.profile, self.book,
+        response = self.run(req.hotkey_create, self.core, self.profile, self.book,
                                      self.hotkey)
 
-        response = req.hotkey_update(self.core, self.profile, self.book,
+        response = self.run(req.hotkey_update, self.core, self.profile, self.book,
                                      self.hotkey)
         assert 'updated' in response['key']
         hotkey2 = ['f11']
-        response = req.hotkey_update(self.core, self.profile, self.book,
+        response = self.run(req.hotkey_update, self.core, self.profile, self.book,
                                      hotkey2)
         assert 'not_found' in response['key']
 
 
     def test_hotkey_load(self):
-        response = list(req.hotkeys_load(self.core, self.profile))
+        response = self.run(req.hotkeys_load, self.core, self.profile)
         assert not response
         dbapi.profile_create(self.core.db, self.profile)
-        response = list(req.hotkeys_load(self.core, self.profile))
+        response = self.run(req.hotkeys_load, self.core, self.profile)
         assert not response
 
         dbapi.hotkey_create(self.core.db, self.profile, self.book, self.hotkey)
         hotkey2 = ['f11']
         dbapi.hotkey_create(self.core.db, self.profile, self.book, hotkey2)
-        response = list(req.hotkeys_load(self.core, self.profile))
+        response = self.run(req.hotkeys_load, self.core, self.profile)
 
         assert response[0]['key'] == 'system_hotkey_register'
         assert response[0]['hotkey'] in (self.hotkey, hotkey2)
@@ -263,13 +270,13 @@ class TestHotkeys(MixIn):
 
 
     def test_hotkeys_reload(self):
-        response = list(req.hotkeys_reload(self.core, 'default', self.profile))
+        response = self.run(req.hotkeys_reload, self.core, 'default', self.profile)
         assert not response
 
         dbapi.profile_create(self.core.db, self.profile)
         dbapi.hotkey_create(self.core.db, 'default', self.book, self.hotkey)
         dbapi.hotkey_create(self.core.db, self.profile, self.book, self.hotkey)
-        response = list(req.hotkeys_reload(self.core, 'default', self.profile))
+        response = self.run(req.hotkeys_reload, self.core, 'default', self.profile)
         assert response[0]['key'] == 'system_hotkey_unregister'
         assert response[1]['key'] == 'system_hotkey_register'
 
@@ -277,7 +284,7 @@ class TestHotkeys(MixIn):
 class TestUiInit(MixIn):
     def test_first_run(self):
         self.core.first_run = True
-        responses = list(req.ui_init(self.core))
+        responses = self.run(req.ui_init, self.core)
         assert 'show_window' == responses[0]['output_method']
         assert 'welcome_screen' == responses[1]['output_method']
         assert 'profile_set_active' == responses[2]['key']
@@ -286,20 +293,20 @@ class TestUiInit(MixIn):
         dbapi.profile_create(self.core.db, self.profile)
         dbapi.hotkey_create(self.core.db, 'default', self.book, self.hotkey)
         dbapi.hotkey_create(self.core.db, self.profile, self.book, self.hotkey)
-        responses = list(req.ui_init(self.core))
+        responses = self.run(req.ui_init, self.core)
         assert responses[3]['key'] == 'system_hotkey_register'
         assert len(responses) == 4
 
     def test_all_other_runs(self):
         self.core.first_run = False
-        responses = list(req.ui_init(self.core))
+        responses = self.run(req.ui_init, self.core)
         assert 'profile_set_active' == responses[0]['key']
         assert len(responses) == 1
 
         dbapi.profile_create(self.core.db, self.profile)
         dbapi.hotkey_create(self.core.db, 'default', self.book, self.hotkey)
         dbapi.hotkey_create(self.core.db, self.profile, self.book, self.hotkey)
-        responses = list(req.ui_init(self.core))
+        responses = self.run(req.ui_init, self.core)
         assert responses[1]['key'] == 'system_hotkey_register'
         assert len(responses) == 2
 
